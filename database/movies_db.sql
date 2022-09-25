064488a4d706ab0748028d9acf4588d861136d72
Sadafpara <sadaf.yazdanparast@cloudreach.com>
2022-09-25 12:08:21 +0200

    Adding 4 tasks of 'Data extact' exercise

--- database/movies_db.sql
+++ database/movies_db.sql
@@ -261,3 +261,40 @@ ORDER BY release_year DESC;
 
 SELECT title, release_year
 FROM movies_tbl;
+
+
+
+
+--------------------------------------------------------
+
+# How many movies did Nolan direct?
+#SELECT * FROM directors_tbl
+SELECT COUNT(*) FROM directors_tbl
+    JOIN movies_tbl ON movies_tbl.director_id = directors_tbl.id 
+WHERE directors_tbl.name LIKE "Christopher Nolan";
+
+#List all the actors born before 1980.
+SELECT * FROM main_actors_tbl
+WHERE year_of_birth < 1980; 
+
+
+#Among all the movies of James Cameron, how many were female actors?
+#SELECT COUNT(*) FROM directors_tbl
+SELECT * FROM directors_tbl
+    JOIN movies_tbl ON movies_tbl.director_id = directors_tbl.id 
+    JOIN movie_actors_tbl ON movie_actors_tbl.movie_id = movies_tbl.id 
+    JOIN main_actors_tbl ON main_actors_tbl.id = movie_actors_tbl.main_actor_id  
+WHERE directors_tbl.name LIKE "James Cameron" 
+AND main_actors_tbl.sex LIKE "F"
+
+
+#How many directors did Leonardo DiCaprio worked with?
+# SELECT * FROM main_actors_tbl
+# SELECT directors_tbl.name, movies_tbl.title FROM main_actors_tbl
+SELECT Count(directors_tbl.name) FROM main_actors_tbl
+    JOIN movie_actors_tbl ON main_actors_tbl.id = movie_actors_tbl.main_actor_id
+    JOIN movies_tbl ON movies_tbl.id = movie_actors_tbl.movie_id 
+    JOIN directors_tbl ON directors_tbl.id = movies_tbl.director_id
+WHERE main_actors_tbl.name LIKE "Leonardo DiCaprio" ;
+
+
